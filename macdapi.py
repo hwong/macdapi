52b15911b38791179b6be460a376b5e8a8162748
Hubert Wong <hubert@hubert.io>
2013-11-13 02:30:49 -0800

    Initial implementation

--- macdapi.py
+++ macdapi.py
@@ -1,10 +1,35 @@
-import os
-from flask import Flask
+import json
+import pandas
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'Hello World!'
+    print 'request.form:', request.form
+    if 'data' not in request.form:
+        return json.dumps({})
+        
+    data = json.loads(request.form['data'])
+
+    short_span_data = pandas.ewma(pandas.Series(data), span=12)
+    long_span_data = pandas.ewma(pandas.Series(data), span=26)
+    macd = short_span_data - long_span_data
+    ewma_macd9 = pandas.ewma(macd, span=9)
+    histogram = macd - ewma_macd9
+
+    result = {
+        'ewma12': list(short_span_data),
+        'ewma26': list(long_span_data),
+        'macd': list(macd),
+        'ewma_macd9': list(ewma_macd9),
+        'histogram': list(histogram)
+    }
+
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
